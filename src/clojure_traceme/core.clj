63340f380ac6506c6e31eae449d243392df68fe5
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2018-04-13 16:30:34 +0300

    Require namespaces before tracing

--- src/clojure_traceme/core.clj
+++ src/clojure_traceme/core.clj
@@ -3,9 +3,15 @@
             [clojure.tools.namespace.find :refer [find-namespaces]]
             [clojure.tools.trace :refer [trace-ns]]))
 
+(defn project-namespaces
+  "Get all namespaces in the current project"
+  []
+  (find-namespaces [(as-file "src/")]))
+
 (defn trace-project-namespaces
   "Start tracing all namespaces in this project (that is, that don't come from dependencies)."
   []
-  (doseq [the-ns (find-namespaces [(as-file "src/")])]
+  (doseq [the-ns (project-namespaces)]
+    (require the-ns)
     (trace-ns the-ns)))
 
